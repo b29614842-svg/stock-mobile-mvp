c928d18a1b5764d2e56908910f5ee16f4bb0b636
b29614842-svg <b29614842@gmail.com>
2026-05-13 16:29:34 +0800

    Update strategy.js

--- api/strategy.js
+++ api/strategy.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  try {
+    const upstream = await fetch('https://script.google.com/macros/s/AKfycbxJgIrVvY4pwPXCjxAF-mlbwgGnjwoD9yCO6ovUqrD60du55B3F1QiJs0U98c3vwMqRLw/exec');
+    const text = await upstream.text();
 
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      error: String(err.message || err)
+    });
+  }
+}
